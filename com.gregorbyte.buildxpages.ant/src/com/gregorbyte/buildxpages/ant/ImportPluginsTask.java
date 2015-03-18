fc3e31e05dada889f2ad1eb79a3fe63451066751
Cameron Gregor <camerongregor@gmail.com>
2015-03-19 08:26:34 +1100

    Just testing out how to do the import to nsf update site

---
+++ com.gregorbyte.buildxpages.ant/src/com/gregorbyte/buildxpages/ant/ImportPluginsTask.java
@@ -0,0 +1,100 @@
+package com.gregorbyte.buildxpages.ant;
+
+import lotus.domino.Agent;
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.NotesException;
+import lotus.domino.NotesFactory;
+import lotus.domino.NotesThread;
+import lotus.domino.Session;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+public class ImportPluginsTask extends Task {
+
+	private static final String AGENT_NAME = "(API)";
+	
+	private boolean deletefirst = false;
+	
+	private String server;
+	private String database;	
+	private String sitexml;
+	
+	public String getServer() {
+		return server;
+	}
+	public void setServer(String server) {
+		this.server = server;
+	}
+	public String getDatabase() {
+		return database;
+	}
+	public void setDatabase(String database) {
+		this.database = database;
+	}
+	public String getSitexml() {
+		return sitexml;
+	}
+	public void setSitexml(String sitexml) {
+		this.sitexml = sitexml;
+	}
+	public boolean isDeletefirst() {
+		return deletefirst;
+	}
+	public void setDeletefirst(boolean deletefirst) {
+		this.deletefirst = deletefirst;
+	}
+	@Override
+	public void execute() throws BuildException {
+
+		log("Server is " + getServer());
+		log("Database is " + getDatabase());
+		log("SiteXml is " + getSitexml());
+		
+		try {
+			
+			NotesThread.sinitThread();
+			
+			Session s = NotesFactory.createSession(); 
+			Database db = s.getDatabase(getServer(), getDatabase());
+			
+			if (db == null || !db.isOpen()) {
+				throw new NullPointerException("Could not get the Update Site Database");
+			}
+			
+			Agent agent = db.getAgent(AGENT_NAME);
+			
+			System.out.println(s.getEffectiveUserName());
+			
+			if (isDeletefirst()) {
+				log("Deleting existing Plugins");
+				Document doc = db.createDocument();			
+				doc.replaceItemValue("command", "deleteall");
+				doc.save();
+				agent.run(doc.getNoteID());
+				doc.recycle();
+				doc = null;
+			}
+
+			Document doc = db.createDocument();
+			
+			doc.replaceItemValue("command", "importsite");
+			doc.replaceItemValue("sitexmlpath", getSitexml());
+			
+			doc.save();			
+			agent.run(doc.getNoteID());
+			
+		} catch (NotesException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("Terminating Notes Thread");
+			NotesThread.stermThread();
+		}
+		
+		super.execute();
+	}
+	
+	
+	
+}
---
+++ com.gregorbyte.buildxpages.ant/src/com/gregorbyte/buildxpages/task/ImportPlugins.java
@@ -0,0 +1,11 @@
+package com.gregorbyte.buildxpages.task;
+
+public class ImportPlugins implements BxTask {
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
